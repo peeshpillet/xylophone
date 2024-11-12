936100c59223d8d47780fea4bb570029fb7a507c
peeshpillet <nicolecfmartin21@gmail.com>
2024-11-12 14:42:22 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
 
-void playSound(int A) {
+void playSound(int A){
   final player = AudioPlayer();
   player.play(AssetSource('note$A.wav'));
 }
@@ -17,146 +17,89 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.red.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.red,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(1);
                     },
-                    child: const Text(
-                      'C',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.orange.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.orange,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(2);
                     },
-                    child: const Text(
-                      'D',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.yellow.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.yellow,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(3);
                     },
-                    child: const Text(
-                      'E',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.green.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.green,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(4);
                     },
-                    child: const Text(
-                      'F',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.teal.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.teal,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(5);
                     },
-                    child: const Text(
-                      'G',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.blue,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(6);
                     },
-                    child: const Text(
-                      'A',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0),
-                  child: TextButton(
+                child: TextButton(
                     style: TextButton.styleFrom(
-                      backgroundColor: Colors.indigo.shade700,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      shadowColor: Colors.black,
-                      elevation: 5,
+                        backgroundColor: Colors.indigo,
+                        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero)
                     ),
                     onPressed: (){
                       playSound(7);
                     },
-                    child: const Text(
-                      'B',
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    child: Container()
                 ),
               ),
             ],
